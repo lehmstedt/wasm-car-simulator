55f88a73399384dae16300ad34aaa9624237a729
Sebastien Lehmstedt <lehmstedt-sebastien@hotmail.fr>
2025-04-05 23:03:06 -0400

    Implemented the camera object to project world position into screen coordinates

--- src/lib.rs
+++ src/lib.rs
@@ -13,12 +13,35 @@ pub struct State {
 }
 
 #[wasm_bindgen]
+pub struct Camera {
+    pub screen_size: i32,
+    pub world_size: i32,
+    pub world_position: i32
+}
+
+#[wasm_bindgen]
+impl Camera {
+    #[wasm_bindgen(constructor)]
+    pub fn new(screen_size: i32, world_size: i32) -> Camera {
+        Camera {
+            screen_size,
+            world_size,
+            world_position: 0
+        }
+    }
+    pub fn project(&self, world_position: i32) -> i32{
+        self.screen_size * (self.world_position + (self.world_size / 2) - world_position)  / self.world_size
+    }
+}
+
+#[wasm_bindgen]
 impl State {
     #[wasm_bindgen(constructor)]
     pub fn new() -> State {
         State {
             position_goal_start: 9000,
             position_goal_end: 10000,
+            position: 500,
             ..State::default()
         }
     }
@@ -42,7 +65,7 @@ impl Default for State {
 pub fn update(current_state: State, throttle: i32) -> State{
     State {
         acceleration: throttle,
-        speed: current_state.speed + current_state.acceleration,
+        speed: (current_state.speed + current_state.acceleration).clamp(0, i32::MAX),
         position: current_state.position + current_state.speed,
         lost: current_state.position > current_state.position_goal_end,
         won: current_state.speed == 0 && current_state.position > current_state.position_goal_start && current_state.position < current_state.position_goal_end,
@@ -176,5 +199,62 @@ mod tests {
         let new_state = update(current_state, 1);
         assert_eq!(0, new_state.position);
         assert_eq!(0, new_state.speed);
+    }
+
+    #[test]
+    fn negative_acceleration_should_not_make_speed_negative(){
+        let current_state = State {
+            acceleration: -1,
+            speed: 0,
+            ..Default::default()
+        };
+
+        let new_state = update(current_state, 0);
+        assert_eq!(0, new_state.speed);
+    }
+
+
+    #[test]
+    fn camera_should_project_900_when_car_world_position_is_100_and_camera_size_is_1000_and_world_and_screen_size_are_the_same(){
+        let camera = Camera {
+            screen_size: 1000,
+            world_size: 1000,
+            world_position: 500,
+        };
+        let screen_position = camera.project(100);
+        assert_eq!(900, screen_position);
+    }
+
+    #[test]
+    fn camera_should_project_900_when_world_position_is_1000_and_camera_world_size_is_10x_bigger_than_screen_size(){
+        let camera= Camera {
+            screen_size: 1000,
+            world_size: 10000,
+            world_position: 5000,
+        };
+        let screen_position = camera.project(1000);
+        assert_eq!(900, screen_position);
+    }
+
+    #[test]
+    fn camera_should_project_900_when_camera_is_at_5000_in_world_position(){
+        let camera = Camera {
+            screen_size: 1000,
+            world_size: 10000,
+            world_position: 5000
+        };
+        let screen_position = camera.project(1000);
+        assert_eq!(900, screen_position);
+    }
+
+    #[test]
+    fn camera_should_project_500_when_camera_is_at_same_world_position_than_target(){
+        let camera = Camera {
+            screen_size: 1000,
+            world_size: 10000,
+            world_position: 1000
+        };
+        let screen_position = camera.project(1000);
+        assert_eq!(500, screen_position);
     }
 }
